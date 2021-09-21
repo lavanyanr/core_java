b0da15671a8f2f6452ba1959dc440223dfd09660
lavanyanr <guddilavinianrao@gmail.com>
2021-09-21 16:59:35 +0530

    writes DataTypesTest program

---
+++ DataTypesTest.java
@@ -0,0 +1,20 @@
+ class DataTypesTest{
+public static void main(String[] arg){
+    byte b=127;
+    System.out.println("b:"+b);
+    short s=30000;
+    System.out.println("s:"+s);
+    int i=100000;
+    System.ot.println("i:"+i);
+    long l=1000000000;
+    System.out.println("l:"+l);
+    float f=10.00f;
+    System.out.println("f:"+f);
+    double d=20.120;
+    System.out.println("d:"+d);
+    char c='2';
+    System.out.println("c:"+c);
+    boolean boo=true;
+    System.out.println("boo:"+boo);
+}
+}
