541d7b4494b655030c494c8ec2b701acb3de9e9c
lavanyanr <guddilavinianrao@gmail.com>
2021-09-23 19:25:15 +0530

    writes DecrementOperator program

---
+++ DecrementOperator.java
@@ -0,0 +1,17 @@
+class DecrementOperator{
+
+public static void main(String[] args){
+  
+     System.out.println("pre decrement operation----------");
+        int a = 10;
+        int b = --a;
+     System.out.println("a:"+a);
+     System.out.println("b:"+b);
+
+     System.out.println("post decrement operation----------");
+        int x = 50;
+        int y = x--;
+     System.out.println("x:"+x);
+     System.out.println("y:"+y);
+}
+}
