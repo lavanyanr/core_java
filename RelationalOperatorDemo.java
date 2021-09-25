a3c0bc60965ef11d96b432a02df286bbbf8cfa26
lavanyanr <guddilavinianrao@gmail.com>
2021-09-25 12:53:13 +0530

    writes RelationalOperatorDemo program

---
+++ RelationalOperatorDemo.java
@@ -0,0 +1,20 @@
+class RelationalOperatorDemo{
+
+public static void main(String[] args){
+  
+        int i = 7;
+        int j = 11;
+        boolean result = i==j;
+        System.out.println("i==j:"+result);
+        boolean result1 = i!=j;
+        System.out.println("i!=j:"+result1);
+        boolean result2 = i>j;
+        System.out.println("i>j:"+result2);
+        boolean result3 = i<j;
+        System.out.println("i<j:"+result3);
+        boolean result4 = i>=j;
+        System.out.println("i>=j:"+result4);
+        boolean result5 = i<=j;
+        System.out.println("i<=j:"+result5);
+}
+}
