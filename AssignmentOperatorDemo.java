7cab02073125dfbe286ff43ecc6e9b032fba52d0
lavanyanr <guddilavinianrao@gmail.com>
2021-09-25 12:43:00 +0530

    writes AssignmentOperatorDemo program

---
+++ AssignmentOperatorDemo.java
@@ -0,0 +1,15 @@
+class AssignmentOperatorDemo{
+
+public static void main(String[] args){
+  
+        int m = 15;
+        int n = 25;
+        n-=m;
+     System.out.println("n:"+n);
+
+        int i = 10;
+        int j = 2;
+        i*=j;
+     System.out.println("i:"+i);
+}
+}
