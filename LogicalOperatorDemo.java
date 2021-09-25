1d72ff0d3ee36c0b6cc8cf342a3acc33ef775b1f
lavanyanr <guddilavinianrao@gmail.com>
2021-09-25 13:04:11 +0530

    writes LogicalOperatorDemo program

---
+++ LogicalOperatorDemo.java
@@ -0,0 +1,14 @@
+class LogicalOperatorDemo{
+
+public static void main(String[] args){
+  
+        int a = 5;
+        int b = 3;
+        System.out.println(a>b && a>b);
+        
+        System.out.println(a>b && a<b);
+
+        System.out.println(a<b || a>b);
+
+}
+}
