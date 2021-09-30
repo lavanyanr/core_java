e642848860f5c87915668c61990288027e1fccf2
lavanyanr <guddilavinianrao@gmail.com>
2021-09-30 21:25:34 +0530

    writes InValidNumber program

---
+++ InValidNumber.java
@@ -0,0 +1,11 @@
+class InValidNumber{
+  public static void main(String[] args) {
+    int num = 20;
+    if (num > 10 ) {
+      System.out.println("The number is valid.");
+    }
+    else {
+      System.out.println("The number is invalid.");
+    }
+  }
+}
