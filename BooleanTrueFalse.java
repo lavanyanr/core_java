cc0574dc2331244cf4e325f918b097f0a5817b7c
lavanyanr <guddilavinianrao@gmail.com>
2021-09-30 21:21:25 +0530

    writes BooleanTrueFalse program

---
+++ BooleanTrueFalse.java
@@ -0,0 +1,13 @@
+class BooleanTrueFalse{
+    public static void main(String[] args) {
+        
+        boolean value = true;
+        if (value) {
+            System.out.println("Hello Developers");
+        }
+        value = false;
+        if (!value) {
+            System.out.println("Hello Jokers");
+        }
+    }
+}
