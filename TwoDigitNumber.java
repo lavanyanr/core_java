96378390c75e2d125a24009d431682c0bd19251a
lavanyanr <guddilavinianrao@gmail.com>
2021-09-30 21:30:38 +0530

    writes TwoDigitNumber program

---
+++ TwoDigitNumber.java
@@ -0,0 +1,16 @@
+class TwoDigitNumber
+{
+    public static void main(String args[])
+    {
+        int n = 13 ;
+        if(n>9 && n<100) 
+        {
+         System.out.println(" 2 digit no. ");
+
+         }
+         else{
+           System.out.println(" it is not 2 digit no. ");
+         }
+        
+    }
+}
