05d63e5b48a71b15eb5aa53abe31568ea4b84a41
lavanyanr <guddilavinianrao@gmail.com>
2021-10-13 00:24:55 +0530

    writes EvenNumDemo program

---
+++ EvenNumDemo.java
@@ -0,0 +1,10 @@
+class EvenNumDemo{
+public static void main(String[] args){
+for(int i=2;i<=20;i++)
+if(i%2==0){
+System.out.println(i);
+}
+for(int j=2;j<=30;j++)
+if(j%3==2){
+System.out.println(j);
+}}}
